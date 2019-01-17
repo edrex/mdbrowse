c610d9451229374a995616ee41bf8023187c618e
Eric Drechsel <eric@pdxhub.org>
2019-01-16 18:02:44 -0800

    module pattern something something

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var md = require('marked')
 
+function mount(target) {
 
-function handleContentString(s) {
-		document.body.innerHTML = md(s);
-}
-function handleNotFound(response) {
-}
+	let handleContentString = (s) => {
+			target.innerHTML = md(s);
+	}
 
-function handleResponse(response) {
-	console.log(response);
-	if (response.ok) {
-		response.text().then(handleContentString);
-	} else {
-		handleContentString(`Error ${response.status}: ${response.statusText}`);
-	}	
-}
+	let handleResponse = (response) =>{
+		console.log(response);
+		if (response.ok) {
+			response.text().then(handleContentString);
+		} else {
+			handleContentString(`Error ${response.status}: ${response.statusText}`);
+		}	
+	}
 
-function handleLocationHash() {
-	console.log(location.hash);
-	if (location.hash.length > 1) {
+	let handleHash = () => {
+		if ( location.hash.length > 1 ) {
 		fetch(location.hash.slice(1))
 			.then(handleResponse)
 			.catch((s) => {console.log(`Fetch error: ${s}`)});
+		}
 	}
+
+	handleHash();
+	window.onhashchange = handleHash;
+	return {};
 }
 
-handleLocationHash()
-window.onhashchange = handleLocationHash;
-//module.exports = app.mount('body')
+module.exports = mount(document.body);
