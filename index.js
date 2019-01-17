60856c46b0178d49ba0443951768a26abf1613a2
Eric Drechsel <eric@pdxhub.org>
2019-01-16 17:05:22 -0800

    set body to response text, response errors

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+var md = require('marked')
+
+
+function handleContentString(s) {
+		document.body.innerHTML = md(s);
+}
+function handleNotFound(response) {
+}
+
+function handleResponse(response) {
+	console.log(response);
+	if (response.ok) {
+		response.text().then(handleContentString);
+	} else {
+		handleContentString(`Error ${response.status}: ${response.statusText}`);
+	}	
+}
+
 function handleLocationHash() {
-	document.body.innerHTML = location.hash;
+	console.log(location.hash);
+	if (location.hash.length > 1) {
+		fetch(location.hash.slice(1))
+			.then(handleResponse)
+			.catch((s) => {console.log(`Fetch error: ${s}`)});
+	}
 }
 
 handleLocationHash()
 window.onhashchange = handleLocationHash;
-module.exports = app.mount('body')
+//module.exports = app.mount('body')
